2f7569d6663047b390b2329858ae3ea8e7481fde
AntonSmuit <a.smirnickiy@smuit.ru>
2020-06-29 13:33:04 +0400

    minor fix

--- server.py
+++ server.py
@@ -23,17 +23,21 @@ assert os.path.exists(ESIA_SETTINGS.get("token_check_key")), "ESIA public key no
 
 app = Flask(__name__)
 
+def generate_esia_login_url():
+    url = client.get_auth_url(ESIA_SETTINGS)
+    print("url: " + url + "\n")
+    return url
+
 
 @app.route("/")
 def index():
-    url = generate_url()
+    url = generate_esia_login_url()
     return "Start here: <a href='" + url + "'>" + url + "</a>"
 
 
 @app.route("/generate_url")
-def generate_url():
-    url = client.get_auth_url(ESIA_SETTINGS)
-    print("url: " + url + "\n")
+def get_esia_login_url():
+    url = generate_esia_login_url()
     return json.dumps({"url": url}, ensure_ascii=False)
 
 
@@ -48,6 +52,7 @@ def process():
     user_id = esia_response_data.get("user_id")
     print("userId: " + user_id)
     inf = client.get_person_main_info(ESIA_SETTINGS, user_id, token)
+    inf["user_id"] = user_id
     # print("info: " + str(inf))
     return json.dumps(inf, ensure_ascii=False)
 
