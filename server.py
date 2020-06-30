cb5573c9f999aba4c50fa6d60c2a0e1f426ad255
AntonSmuit <a.smirnickiy@smuit.ru>
2020-06-30 14:42:58 +0400

    add logging

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import json
 import os
-import pprint
+import logging
 
 from dotenv import load_dotenv
 
@@ -11,21 +11,39 @@ from flask import Flask, request
 
 from esia_connector import client
 
+app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
 # for item, value in os.environ.items():
 #     print(f"{item}: {value}")
 
+# logger = logging.getLogger('werkzeug')
+# access_handler = logging.FileHandler(os.path.join("logs", "access.log"))
+# logger.addHandler(access_handler)
+# app.logger.addHandler(handler)
+os.makedirs('logs', exist_ok=True)
+
+fileHandler = logging.FileHandler(os.path.join("logs", "app.log"))
+fileHandler.setLevel(logging.DEBUG)
+# fileHandler.setFormatter(utils.LOGGING_FORMATTER)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+# streamHandler.setFormatter(utils.LOGGING_FORMATTER)
+app.logger.addHandler(fileHandler)
+app.logger.addHandler(streamHandler)
+app.logger.info("Logging is set up.")
+
 ESIA_SETTINGS = client.init_esia_conn_settings()
-pprint.pprint(ESIA_SETTINGS)
+logger.info(str(ESIA_SETTINGS))
 
 assert os.path.exists(ESIA_SETTINGS.get("certificate_file")), "public key not found!"
 assert os.path.exists(ESIA_SETTINGS.get("private_key_file")), "private key not found!"
 assert os.path.exists(ESIA_SETTINGS.get("token_check_key")), "ESIA public key not found!"
 
-app = Flask(__name__)
 
 def generate_esia_login_url():
     url = client.get_auth_url(ESIA_SETTINGS)
-    print("url: " + url + "\n")
+    logger.info("url: " + url + "\n")
     return url
 
 
@@ -46,11 +64,13 @@ def process():
     code = request.args.get('code')
     state = request.args.get('state')
     error = request.args.get('error_description')
-    print(error)
+    logger.info("esia code : " + str(code or ''))
+    logger.info("esia state : " + str(state or ''))
+    logger.error("esia error : " + str(error or ''))
     esia_response_data = client.complete_authorization(ESIA_SETTINGS, code, state, validate=False)
     token = esia_response_data.get("token")
     user_id = esia_response_data.get("user_id")
-    print("userId: " + user_id)
+    logger.info("userId: " + str(user_id or ''))
     inf = client.get_person_main_info(ESIA_SETTINGS, user_id, token)
     inf["esia_user_id"] = user_id
     # print("info: " + str(inf))
